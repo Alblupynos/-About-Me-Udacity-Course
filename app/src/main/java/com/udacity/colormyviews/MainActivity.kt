5a3f49c45e3ffce71a5483a8a0fe6c18b9d588b3
Albina <alblupynos@gmail.com>
2020-03-02 18:58:03 +0200

    Constraint Layout clickable boxes implementation

--- app/src/main/java/com/udacity/colormyviews/MainActivity.kt
+++ app/src/main/java/com/udacity/colormyviews/MainActivity.kt
@@ -1,12 +1,45 @@
 package com.udacity.colormyviews
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.udacity.colormyviews.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        setListeners()
+
+    }
+
+    private fun setListeners() {
+        val clikableViews: List<View> =
+            listOf(box_one_text, box_two_text, box_three_text,
+                box_four_text, box_five_text, constraint_layout)
+        for (item in clikableViews) {
+            item.setOnClickListener {makeColored(it)}
+        }
+    }
+
+    fun makeColored(view: View) {
+        when (view.id) {
+
+            // Boxes using Color class colors for background
+            binding.boxOneText.id -> view.setBackgroundColor(Color.DKGRAY)
+            binding.boxTwoText.id-> view.setBackgroundColor(Color.GRAY)
+
+            // Boxes using Android color resources for background
+            binding.boxThreeText.id -> view.setBackgroundResource(android.R.color.holo_green_light)
+            binding.boxFourText.id -> view.setBackgroundResource(android.R.color.holo_green_dark)
+            binding.boxFiveText.id -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+            else -> view.setBackgroundColor(Color.LTGRAY)
+        }
     }
 }
